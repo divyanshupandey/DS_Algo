b75c9b8e7feb5a41ddc76b947a5b78423693d6d1
Divyanshu Pandey <pandey.divyanshu34@gmail.com>
2020-06-16 19:19:14 +0530

    Update RB_Tres.py

--- RB_Tres.py
+++ RB_Tres.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Feb  8 14:29:00 2019
-
-@author: dprasad@ad.msystechnologies.com
-"""
 
 with open('/home/ad.msystechnologies.com/dprasad/Downloads/vocab.vi') as f:
     mylist = [line.rstrip('\n') for line in f]
