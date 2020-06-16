1246ba6aad4b8f7891f9767188e09ac49ece6e29
Divyanshu Pandey <pandey.divyanshu34@gmail.com>
2020-06-16 19:21:53 +0530

    Update RB_Tres.py

--- RB_Tres.py
+++ RB_Tres.py
@@ -1,5 +1,5 @@
 
-with open('/home/ad.msystechnologies.com/dprasad/Downloads/vocab.vi') as f:
+with open('/home/Downloads/vocab.vi') as f:
     mylist = [line.rstrip('\n') for line in f]
 
 dictOfWords = { i : 0 for i in mylist }
